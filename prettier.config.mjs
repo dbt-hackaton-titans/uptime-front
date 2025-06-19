56816be9f172a105ab9185c0c6f9200c40813dd4
Monassi Carrasco <mon4ssi@outlook.com>
2025-06-19 17:42:03 -0500

    chore(framework): add prettier configuration file

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('prettier').Config} */
+const config = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+};
+
+export default config;
